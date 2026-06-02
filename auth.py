36fbd1505a8846fa2daf9d3cdf7dfbf0901e6a47
lrssb <2835224542@qq.com>
2026-06-02 20:55:27 +0800

    Rename login function to authenticate_user

--- auth.py
+++ auth.py
@@ -1,5 +1,7 @@
-def login(username, password):
-    """用户登录函数"""
+def authenticate_user(username, password):
+    """用户认证函数"""
+    if not username or not password:
+        return False
     if username == "admin" and password == "123456":
         return True
     return False
